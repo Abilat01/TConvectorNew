2ed8415d66220b99548ad19c2fc7079df3456492
Abilat01 <Abilat01@yandex.ru>
2021-09-20 13:11:42 +0300

    Orientation

--- TConvectorNew/ViewController.swift
+++ TConvectorNew/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var farentheitLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sladerAction(_ sender: UISlider) {
+    }
+    
 }
 
