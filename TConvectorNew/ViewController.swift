8b1be5067d73a00184fa19e64cba4180d7439d8a
Abilat01 <Abilat01@yandex.ru>
2021-09-21 10:13:28 +0300

    Готово

--- TConvectorNew/ViewController.swift
+++ TConvectorNew/ViewController.swift
@@ -12,6 +12,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var farentheitLabel: UILabel!
+    @IBOutlet weak var slaider: UISlider! {
+        didSet {
+            slaider.maximumValue = 100
+            slaider.minimumValue = 0
+            slaider.value = 0
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +26,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sladerAction(_ sender: UISlider) {
+        
+        let tempiratureCelsius = Int(round(sender.value))
+        celsiusLabel.text = "\(tempiratureCelsius)ºC"
+        let farinheitTempirature = Int(round(sender.value * 9 / 5)) + 32
+        farentheitLabel.text = "\(farinheitTempirature)ºF"
+        
     }
     
 }
